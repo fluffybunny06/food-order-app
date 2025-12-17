3c7e7368579ec345d346903169b922d3cbfbe7db
fluffybunny06 <yiphuenzhang@gmail.com>
2025-12-17 16:04:29 -0500

    fix(header): fix NavLink routing typo

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -22,7 +22,7 @@ export default function MainHeader() {
               <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <NavLink href="community">Foodies Community</NavLink>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
